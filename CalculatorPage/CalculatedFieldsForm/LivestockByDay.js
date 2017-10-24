58c3dd9aa0fd3f627570cd5b99d10e7c470ad755
wangcan022 <32725250+wangcan022@users.noreply.github.com>
2017-10-24 11:58:16 +1100

    Create LivestockByDay.js

---
+++ CalculatorPage/CalculatedFieldsForm/LivestockByDay.js
@@ -0,0 +1,8 @@
+(function(){
+if (fieldname13>=0 && fieldname5 >=0 && fieldname6 >=0 && fieldname7 >=0 && fieldname15 >=0 && fieldname17 >=0 && fieldname19 >=0 && fieldname20 >=0 && fieldname21 >=0 && fieldname22 >=0 && fieldname23 >=0 && fieldname24 >=0 && fieldname25 >=0 && fieldname2 >=0 && fieldname3 >=0 && fieldname4 >=0)
+
+return prec((ceil(fieldname13)*4) + (ceil(fieldname5)*80) + (ceil(fieldname6)*100) + (ceil(fieldname7)*55) + (ceil(fieldname15)*0.34) + (ceil(fieldname17)*1.2) +
+ (ceil(fieldname19)*2.8) + (ceil(fieldname20)*10) + (ceil(fieldname21)*16) + (ceil(fieldname22)*12) + (ceil(fieldname23)*18.5) + (ceil(fieldname24)*20) +
+ (ceil(fieldname25)*20) + (ceil(fieldname2)*6) + 
+(ceil(fieldname3)*10) + (ceil(fieldname4)*4),0)
+})();
