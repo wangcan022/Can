f210479e897a1f6450b5a6e6f58288250bc46103
wangcan022 <32725250+wangcan022@users.noreply.github.com>
2017-10-24 12:05:16 +1100

    Create CropsWaterConsumptionSpring.js

---
+++ CalculatorPage/CalculatedFieldsForm/CropsWaterConsumptionSpring.js
@@ -0,0 +1,5 @@
+(function(){
+if (fieldname2>=0 && fieldname3>=0 && fieldname4>=0 && fieldname5>=0 && fieldname6>=0 && fieldname7>=0 && fieldname13>=0 && fieldname15>=0 && fieldname17>=0 && fieldname19>=0 && fieldname20>=0 && fieldname21>=0 && fieldname22>=0 && fieldname23>=0 && fieldname24>=0 && fieldname25>=0 && fieldname28>=0 && fieldname29>=0 && fieldname30>=0 && fieldname31>=0 && fieldname32>=0 && fieldname39>=0 && fieldname40>=0 && fieldname41>=0 && fieldname42>=0 && fieldname43>=0 && fieldname44>=0 && fieldname54>=0)
+
+return prec(abs(fieldname13*600 + fieldname5*425 + fieldname41*425 + fieldname40*425 + fieldname39*425 + fieldname44*275 + fieldname43*213 + fieldname17*350 + fieldname19*500 + fieldname20*650 + fieldname23*300 + fieldname24*425 + fieldname25*375 +fieldname54*300 + fieldname3*338 + fieldname29*325 + fieldname30*667),2)
+})();
