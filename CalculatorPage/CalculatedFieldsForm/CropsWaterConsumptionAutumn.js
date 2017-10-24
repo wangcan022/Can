820edace77702b9795fc418bd62adae3dfc9653b
wangcan022 <32725250+wangcan022@users.noreply.github.com>
2017-10-24 12:04:06 +1100

    Create CropsWaterConsumptionAutumn.js

---
+++ CalculatorPage/CalculatedFieldsForm/CropsWaterConsumptionAutumn.js
@@ -0,0 +1,5 @@
+(function(){
+if (fieldname2>=0 && fieldname3>=0 && fieldname4>=0 && fieldname5>=0 && fieldname6>=0 && fieldname7>=0 && fieldname13>=0 && fieldname15>=0 && fieldname17>=0 && fieldname19>=0 && fieldname20>=0 && fieldname21>=0 && fieldname22>=0 && fieldname23>=0 && fieldname24>=0 && fieldname25>=0 && fieldname28>=0 && fieldname29>=0 && fieldname30>=0 && fieldname31>=0 && fieldname32>=0 && fieldname39>=0 && fieldname40>=0 && fieldname41>=0 && fieldname42>=0 && fieldname43>=0 && fieldname44>=0 && fieldname54>=0)
+
+return prec(abs(fieldname5*425 + fieldname41*425 + fieldname40*425 + fieldname39*425 + fieldname6*275 + fieldname7*400 + fieldname15*213 + fieldname42*213 + fieldname17*350 + fieldname21*250 + fieldname22*225 + fieldname2*300 + fieldname4*275 + fieldname28*338 + fieldname30*667 + fieldname31*400 + fieldname32*300),2)
+})();
